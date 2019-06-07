906b4edd181a04b7c73b77fd9fe0a4a4bc2369de
satinder1208 <satinder.singh.81@gmail.com>
2019-06-07 16:47:36 +0530

    hello test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use('/builds',express.static(__dirname + '/builds'));
 

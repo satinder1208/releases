5e6fb080477860f58047afcfb9e9ce2bb96ebb4b
satinder1208 <satinder.singh.81@gmail.com>
2019-06-07 15:52:16 +0530

    scrips

---
+++ build.sh
@@ -0,0 +1,15 @@
+current_dir=$(pwd)
+script_dir=$(dirname $0)
+
+if [ $script_dir = '.' ]
+then
+script_dir="$current_dir"
+fi
+
+cd $script_dir
+git fetch --all
+git reset --hard origin/master
+
+echo "Fetch complete"
+
+pm2 start start.sh
---
+++ start.sh
@@ -0,0 +1,10 @@
+current_dir=$(pwd)
+script_dir=$(dirname $0)
+
+if [ $script_dir = '.' ]
+then
+script_dir="$current_dir"
+fi
+cd $script_dir
+cd ..
+node server.js

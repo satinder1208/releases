ffbc2ec3ea697278f67ad4953bd36f274810efd2
satinder1208 <satinder.singh.81@gmail.com>
2019-06-17 14:38:59 +0530

    cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-
+var cors = require('cors')
 const app = express()
-
+app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use('/builds',express.static(__dirname + '/builds'));

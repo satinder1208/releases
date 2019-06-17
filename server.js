0cbc8983d6f474d6ff9d330bfdf2735ae40fccaa
satinder1208 <satinder.singh.81@gmail.com>
2019-06-17 14:47:31 +0530

    cors

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
-var cors = require('cors')
 const app = express()
-app.use(cors())
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use('/builds',express.static(__dirname + '/builds'));

b0409db673d9dfcfda0f1c34a43caf754227409c
satinder1208 <satinder.singh@violetpath.in>
2020-08-09 19:30:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use('/builds',express.static(__dirname + '/builds'));
 
-app.listen(3000, () => console.log('Server running on port 3000!')) 
+app.listen(3999, () => console.log('Server running on port 3999!')) 
